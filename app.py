50c5eff4fd2dccd3037534518921adc669b16854
Agneya T <agneyat2@gmail.com>
2023-11-18 01:46:36 -0500

    empty competition can be generated

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from app_utilities import create_folder, create_empty_comp
 
 app = Flask(__name__)
+
+INIT_COMP = False
 
 @app.route("/")
 @app.route("/index")
@@ -9,7 +12,35 @@ def index():
 
 @app.route("/create")
 def create():
-	return render_template("create.html")
+	global INIT_COMP
+	if (INIT_COMP == False):
+		return render_template('initcomp.html')
+	else:
+		return render_template("create.html")
+
+@app.route("/edithome")
+def edithome():
+	return render_template("edithome.html")
+
+@app.route("/initcomp", methods=('GET', 'POST'))
+def initcomp():
+
+
+	# Check if comp has already been made, and if it has then dont allow access to initcomp again
+	global INIT_COMP
+	# if request.method == 'POST' and INIT_COMP == False:
+	if request.method == 'POST':
+		compname = request.form['compname']
+		compauthor = request.form['compauthor']
+		compskill = request.form['compskill']
+		compdescription = request.form['compdescription']
+
+		# Create empty comp
+		create_empty_comp(compname,compauthor,compskill,compdescription)
+		INIT_COMP = True
+		return redirect(url_for('create'))
+	else:
+		return redirect(url_for('create'))
 
 if __name__ == '__main__':
 	app.run(debug=True)
---
+++ app_utilities.py
@@ -0,0 +1,56 @@
+import os
+import ruamel.yaml
+
+# Creates a folder anywhere in the chalgen-online folder, paths deeper must be specified
+def create_folder(name):
+    current_directory = os.getcwd()
+    new_folder_path = os.path.join(current_directory, name)
+    if not os.path.exists(new_folder_path):
+        os.mkdir(new_folder_path)
+    else:
+        print("CRITICAL: Folder path already exists")
+
+# Creates a file anywhere in the chalgen-online folder, paths deeper must be specified
+def create_file(name,text):
+    current_directory = os.getcwd()
+    new_folder_path = os.path.join(current_directory, name)
+    with open(new_folder_path,'w') as file:
+        # If theres text write it, if not just put nothing in it
+        if (text):
+            file.write(text)
+        else:
+            pass
+
+# Creates a file anywhere in the chalgen-online folder, paths deeper must be specified
+def create_yaml(name,data):
+    yaml = ruamel.yaml.YAML()
+    yaml.indent(offset=2)
+
+    with open(name, 'w') as yaml_file:
+        yaml.dump(data, yaml_file)
+
+def create_empty_comp(name,author,skill,description):
+    comp_dir = os.path.join('competitions', name)
+    create_folder(comp_dir)
+    create_folder(os.path.join(comp_dir,"chals"))
+
+    text = f"""# Welcome to CTFg!
+
+## {name}
+
+Author: {author}, Competition difficulty: {skill}
+
+## Challenge Description: 
+{description}
+
+Competition generated using CHALGEN-Online - Created by Agneya Tharun
+"""
+
+    create_file(os.path.join('competitions',name,'Home.md'),text)
+    data = {
+        'entrypoint': ['Fesbuc'],
+        'admin_email': 'admin@admin.com',
+        'admin_password': 'ctfgadministrator',
+        'homepage': 'Home.md'
+    }
+    create_yaml(os.path.join('competitions',name,'config.yaml'),data)
